e3e5af7c4379f4b9a9618d8e722f66a598a40972
nanawebdev <nanaprettysinger@gmail.com>
2021-05-21 14:16:45 +0300

    обавялет мобильную версию

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,18 @@
 const videoMenuItems = document.querySelectorAll('.video-menu__item')
-// const videoMenuItemCurrent = document.querySelectorAll('.video-menu__item--current')
+
+const firstVideoEl = document.querySelector('.video-menu__item--current .video-menu__media')
+firstVideoEl.play()
 
 for (let i = 0; i < videoMenuItems.length; i++) {
   const videoMenuItem = videoMenuItems[i]
 
-  videoMenuItem.addEventListener('click', function ()  {
-    previosVideoCurrent = document.querySelector('.video-menu__item--current')
-    previosVideoCurrent.classList.remove('video-menu__item--current')
+  videoMenuItem.addEventListener('click', function () {
+    previousVideoCurrent = document.querySelector('.video-menu__item--current')
+    const previousVideoEl = previousVideoCurrent.querySelector('.video-menu__media')
+    previousVideoEl.pause()
+    previousVideoCurrent.classList.remove('video-menu__item--current')
     videoMenuItem.classList.add('video-menu__item--current')
+    const videoEl = videoMenuItem.querySelector('.video-menu__media')
+    videoEl.play()
   })
 }
