72994ad3086af14d1bcd5ce30fad5ee2e9a7a205
nanawebdev <nanaprettysinger@gmail.com>
2021-06-30 22:32:56 +0300

    Фиксы слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,36 +1,33 @@
-// const videoMenuItems = document.querySelectorAll('.video-menu__item')
-
-// const firstVideoEl = document.querySelector('.video-menu__item--current .video-menu__media')
-// firstVideoEl.play()
-
-// for (let i = 0; i < videoMenuItems.length; i++) {
-//   const videoMenuItem = videoMenuItems[i]
-
-//   videoMenuItem.addEventListener('click', function () {
-//     previousVideoCurrent = document.querySelector('.video-menu__item--current')
-//     const previousVideoEl = previousVideoCurrent.querySelector('.video-menu__media')
-//     previousVideoEl.pause()
-//     previousVideoCurrent.classList.remove('video-menu__item--current')
-//     videoMenuItem.classList.add('video-menu__item--current')
-//     const videoEl = videoMenuItem.querySelector('.video-menu__media')
-//     videoEl.play()
-//   })
-// }
-
 const sliderVideos = document.querySelectorAll('.slider__video')
-const sliderDescriptions = document.querySelectorAll('.slider__description')
 
-sliderVideos[0].play()
-for (let i = 0; i < 4; i++) {
-  sliderVideos[i].addEventListener('ended', () => {
-    sliderVideos[i].classList.remove('slider__video--current')
-    sliderDescriptions[i].classList.remove('slider__description--current')
+if (sliderVideos.length > 0) {
+  const sliderDescriptions = document.querySelectorAll('.slider__description')
 
-    const nextIndex = i + 1 === 4 ? 0 : i + 1
+  sliderVideos[0].play()
+  for (let i = 0; i < 4; i++) {
+    sliderVideos[i].addEventListener('ended', () => {
+      sliderVideos[i].classList.remove('slider__video--current')
+      sliderDescriptions[i].classList.remove('slider__description--current')
 
-    sliderVideos[nextIndex].classList.add('slider__video--current')
-    sliderDescriptions[nextIndex].classList.add('slider__description--current')
-    sliderVideos[nextIndex].play()
-  });
+      const nextIndex = i + 1 === 4 ? 0 : i + 1
+
+      sliderVideos[nextIndex].classList.add('slider__video--current')
+      sliderDescriptions[nextIndex].classList.add('slider__description--current')
+      sliderVideos[nextIndex].play()
+    });
+  }
 }
 
+const mainOffsetTop = document.querySelector('.main').offsetTop
+const videoWrapper = document.querySelector('.video')
+const videoBgEl = document.querySelector('.video__media')
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= mainOffsetTop) {
+    videoBgEl.pause()
+    videoWrapper.style.position = 'absolute'
+  } else {
+    videoBgEl.play()
+    videoWrapper.style.position = 'fixed'
+  }
+})
